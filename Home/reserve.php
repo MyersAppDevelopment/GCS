a90cfdbeb02191ba693e1107ba72ce546002b77f
Rob Myers <rob.myers@gmail.com>
2015-04-25 19:23:52 -0400

    Setup Reservation Model Population
    Initial insertion of date and golf item id into model is setup, but no
    checking for already reserved dates, etc.

---
+++ Home/reserve.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+$golfItemId = $_SESSION['golfItemId'];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (empty($_POST["reserveDate"])) {
+		$reserveDateERR = "Requested reserve date required.";
+    echo $reserveDateERR;
+	}
+	else {
+		$reserveDateInitial = test_input($_POST["reserveDate"]);
+		date_default_timezone_set('America/New_York');
+		$reserveDate = date("Y-m-d", strtotime($reserveDateInitial));
+		}
+}
+
+	function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+$servername = "localhost";
+$username = "rmyers7";
+$password = "testpw";
+$dbname = "golfclubsharingtestdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$insertGolfBagIntoReserveModel = "INSERT INTO reservedclubsmodel (golfItems_id, dateReserved) VALUES ('$golfItemId','$reserveDate')";
+
+if ($conn->query($insertGolfBagIntoReserveModel) === TRUE) {
+    echo "New reservation created.";
+    unset($golfItemId);
+}
+else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
